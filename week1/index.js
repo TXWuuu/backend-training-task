6b9017093bc2dac7ddf7ec6eb5500fe2918af0fe
TX <zoe.th.wu@tw.ey.com>
2025-01-22 16:39:48 +0800

    homeWork Update

--- week1/index.js
+++ week1/index.js
@@ -8,6 +8,10 @@
 
 // 1.1 範例
 const alexAge = 25;
+const alexMembershipID = "GYM2024-12345";
+const isRunningOnTreadmill = true;
+console.log(`[Q1] alexAge: ${alexAge}, alexMembershipID: ${alexMembershipID}, isRunningOnTreadmill: ${isRunningOnTreadmill}`);
+
 
 // ### 題目二：變數命名練習
 // - 瑜伽團課 - 300 元
@@ -16,10 +20,10 @@ const alexAge = 25;
 // 情境：Alex 這個月的運動預算有 3000 元
 // 請修改以下中文變數名稱，讓他符合變數語意
 
-const 變數一 = 300;
-const 變數二 = 500;
-const 變數三 = 1500;
-const 變數四 = 3000;
+const yogaGroupClassPrice = 300;
+const workoutGroupClassPrice = 500;
+const workout1On1Price = 1500;
+const campaignBudget = 3000;
 
 // ### 題目三：變數計算
 // 呈上題，Alex 想要規劃好自己的運動課程，需符合以下三個條件，請將花費總數計算在 AlexBudget 上，一起幫幫他吧！
@@ -27,17 +31,32 @@ const 變數四 = 3000;
 // 條件二：瑜伽團課只能一次買 3 堂
 // 條件三：一定要花到 2400 以上，並購買 5 堂課程
 
+const AlexBudget = campaignBudget - (yogaGroupClassPrice*3 + workoutGroupClassPrice + workout1On1Price);
 console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 // ### 題目四：線稿圖截圖，看圖宣告變數
 // 請參考資料夾內 q4.webp 圖片
 // 請依照你看到的內容來嘗試設計變數和值（至少 3 個）
 
+const classNum = 14;
+const classTime = 50;
+const avgPrice = 180;
+const discount = 0.9;
+
+
+
 // ### 題目五：布林值與變數定義，看是否有用對 const, let
 // 情境：Alex 在往健身房的路上，望向城市的風景，請描述她看到的一切，並宣告變數與賦予值
 // 4-1. Alex 在等紅綠燈，他抬頭看一下現在是紅燈，還有 28 秒綠燈（最多 3 個宣告）
+let trafficSignal = 'red';
+let waitingSeconds = 28 ;
+let moveForward = false;
+
 // 4-2. 目前一起等待的機車有 8 台
+let waitMotocycleNum = 8;
 // 4.3. Alex 望向天空，看到天上有 5 朵白雲和 1 顆太陽
+const sunNum = 1;
+const cloudNum = 8;
 
 // ### 題目六：情境題：簡單變數計算
 // 情境：Alex 每天都會帶著 2000cc 的水壺
@@ -50,6 +69,9 @@ console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 let myWater = 2000; // 水壺容量
 myWater -= 500; // 早上喝了 500cc
+myWater -= 800;
+myWater += 1000;
+myWater -= 700;
 
 console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
@@ -62,7 +84,13 @@ console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
 let totalBill = 0;
 const machineUsePrice = 50;
-
+const groupClassPrice = 150;
+const machineUsimgTime = 3;
+const weekClass = 2;
+const weekMachineUse = machineUsePrice * machineUsimgTime;
+const weekGroupClass = groupClassPrice * weekClass;
+totalBill += weekMachineUse + weekGroupClass;
+const total = totalBill;
 console.log(
   `Anna 本週器械使用費共 ${machineUsePriceTotal} 元，團體課費用共 ${groupClassesTotal} 元，一共消費金額是 ${total}元`
 );
@@ -71,11 +99,12 @@ console.log(
 // 情境：請依照以下程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
 // 以下程式碼請勿變更
 let a = 8; // 範例：宣告了一個 a 的變數，並賦予了一個 8 的數字型別
-let b = 0;
-a = 13;
-a = b + 4;
-a - b;
-b += 1;
+let b = 0; // 宣告了一個 b 的變數，並賦予了一個 0 的數字型別
+a = 13; // 變數a重新賦予了一個 13 的數字型別
+a = b + 4; // 取得變數b＝0，0+4的結果為4，將變數a重新賦予一個 4 的數字型別
+a - b; //取得變數a＝4,b=0，執行4-0
+b += 1; //取得變數b＝0，0+1得結果為1，將變數b重新賦予一個 1 的數字型別
+//最終 a值為4，b值為1
 
 // ### 題目九：型別查詢
 // 請不要觀看 console.log，透過註解告知解答每個變數的型別
@@ -88,7 +117,11 @@ let h = f + g;
 
 // 請從以下新增註解，告知上面每行各別是哪些型別
 // a 是 string
-// b 是 ???
+// d 是 number
+// e 是 string
+// f 是 boolean
+// g 是 number
+// h 是 number
 
 // ### 題目十：傳值與傳參考
 // 情境：請依照程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
@@ -99,3 +132,10 @@ let numberArr2 = numberArr1;
 numberArr2.push(20);
 numberArr2 = [25, 30, 35];
 console.log(numberArr1, numberArr2);
+
+
+//宣告一變數numberArr1，並賦予一數字型別陣列[5, 10, 15]
+//宣告一變數numberArr2，指向numberArr1的記憶體空間[5, 10, 15]
+//numberArr2 增加一陣列元素 [5, 10, 15, 20]
+//賦予變數numberArr2一數字型別陣列[25, 30, 35]，並為一個新的記憶體空間
+//numberArr1 值為[5, 10, 15, 20]，numberArr2 值為[25, 30, 35]
